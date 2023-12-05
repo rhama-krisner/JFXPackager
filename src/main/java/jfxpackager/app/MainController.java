ce254f7d4d51b3f140c8fbe994cd787690de0fc0
Rhama Krisner <rhamakrisner@gmail.com>
2023-12-05 12:02:47 -0300

    Add new ui to main project

--- src/main/java/jfxpackager/app/MainController.java
+++ src/main/java/jfxpackager/app/MainController.java
@@ -1,26 +1,16 @@
 package jfxpackager.app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @FxmlView("MainController.fxml")
 public class MainController {
-    @FXML
-    private Label label_teste;
-    @FXML
-    private Button button_setLabelText;
-    @FXML
-    private Button button_clearLabelText;
+
 
     @FXML
     private void initialize() {
-        button_setLabelText.setOnAction(view -> label_teste.setText("Está funcionando essa bagaça!"));
 
-        button_clearLabelText.setOnAction(view -> label_teste.setText(""));
     }
 }
