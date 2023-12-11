efcf835d7109824ebfed8cdebc144b64a6abb7dd
Rhama Krisner <rhamakrisner@gmail.com>
2023-12-11 17:23:50 -0300

    Add propertie reader for theme selection like a cache memory

---
+++ src/main/java/jfxpackager/app/util/LerPropertie.java
@@ -0,0 +1,40 @@
+package jfxpackager.app.util;
+
+import java.io.*;
+import java.util.Properties;
+
+public class LerPropertie {
+    private String nomeDoPropertie = "src/main/resources/theme.properties";
+    private Properties props = new Properties();
+
+    public void setPropertyTheme(String theme) throws IOException {
+        switch (theme) {
+            case "light" -> props.setProperty("theme", "light");
+            case "dark" -> props.setProperty("theme", "dark");
+        }
+        FileOutputStream os = new FileOutputStream(nomeDoPropertie);
+        props.store(os,null);
+    }
+
+    public String getProperties() throws IOException {
+        FileInputStream in = new FileInputStream(nomeDoPropertie);
+        props.load(in);
+        String theme = props.getProperty("theme");
+
+        switch (theme){
+            case "light" -> {
+                return "light";
+            }
+            case "dark" -> {
+                return "dark";
+            }
+        }
+
+        return theme;
+    }
+
+    public static void main(String[] args) throws IOException {
+        LerPropertie lerPropertie = new LerPropertie();
+        System.out.println(lerPropertie.getProperties());
+    }
+}
